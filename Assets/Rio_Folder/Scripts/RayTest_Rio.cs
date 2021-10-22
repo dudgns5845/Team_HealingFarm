6597bcb944fc9a468ca9129841a24fe18eb276f0
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2021-10-23 03:13:42 +0900

    Update Rio

---
+++ Assets/Rio_Folder/Scripts/RayTest_Rio.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTest_Rio : MonoBehaviour
+{
+    Animator animator;
+    private void Start()
+    {
+       
+    }
+}
