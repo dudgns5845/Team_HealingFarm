bce9c327db33ab00e26be20d0680a602fb1a6eb1
hyojin-ahn <angela991014@naver.com>
2021-11-04 17:24:03 +0900

    1104

--- Assets/Hyo_Folder/Script/DataBaseManager.cs
+++ Assets/Hyo_Folder/Script/DataBaseManager.cs
@@ -7,6 +7,31 @@ using Firebase.Auth;
 using System;
 
 [Serializable]
+public class EndTime
+{
+    public int myCurrCoin;
+    //public DateTime exitTime;
+    // mm/dd/yyyy hh:mm:ss
+
+    public int month;
+    public int day;
+    public int year;
+    public int hour;
+    public int minute;
+    public int second;
+
+    public EndTime(int month, int day, int year, int hour, int minute, int second)
+    {
+        this.month = month;
+        this.day = day;
+        this.year = year;
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+}
+
+[Serializable]
 public class User
 {
 
@@ -60,7 +85,84 @@ public class DataBaseManager : MonoBehaviour
         dataBase = FirebaseDatabase.DefaultInstance;
     }
 
+    //Save time , coin 
+    public EndTime exitTimeJson;
+   
+    public void SaveTimeCoin(Action<bool> complete)
+    {
+        StartCoroutine(ISaveTimeCoin(complete));
+    }
+    IEnumerator ISaveTimeCoin(Action<bool> complete)
+    {
+        string json =JsonUtility.ToJson(exitTimeJson);
 
+        //경로
+        string path = "USER_INFO/" + FirebaseAuth.DefaultInstance.CurrentUser.UserId+"/MyCoin";
+        var task = dataBase.GetReference(path).SetRawJsonValueAsync(json);
+        yield return new WaitUntil(() => task.IsCompleted);
+        if (task.Exception == null)
+        {
+            print("시간 저장 완료");
+            complete(true);
+        }
+        else
+        {
+            print("시간 저장 실패 : " + task.Exception);
+            complete(false);
+        }
+    }
+    //Get Time
+    public void GetTimeCoin(Action<bool> complete)
+    {
+        StartCoroutine(IGetTimeCoin(complete));
+    }
+    IEnumerator IGetTimeCoin(Action<bool> complete)
+    {
+        string path = "USER_INFO/" + FirebaseAuth.DefaultInstance.CurrentUser.UserId+ "/MyCoin";
+        var task = dataBase.GetReference(path).GetValueAsync();
+        yield return new WaitUntil(() => task.IsCompleted);
+        if (task.Exception == null)
+        {
+            if (task.Result.Exists)
+            {
+                print(task.Result.GetRawJsonValue());
+                print("코인 정보 로드 완료");
+                exitTimeJson = JsonUtility.FromJson<EndTime>(task.Result.GetRawJsonValue());
+                //Debug.Log(exitTimeJson.exitTime);
+                //Debug.Log(exitTimeJson.myCurrCoin);
+                //Debug.Log(exitTimeJson.year);
+                //Debug.Log(exitTimeJson.month);
+                //Debug.Log(exitTimeJson.day);
+                //Debug.Log(exitTimeJson.hour);
+                //Debug.Log(exitTimeJson.minute);
+                DateTime testDate = SetDate(exitTimeJson);
+                Debug.Log(testDate);
+                complete(true);
+            }
+            else
+            {
+                print("코인 정보 로드 실패" + task.Exception);
+
+                GetTimeCoin((result) =>
+                {
+                    complete(true);
+                });
+
+            }
+        }
+    }
+
+    public DateTime SetDate(EndTime endTimeValue)
+    {
+        DateTime temp = new DateTime(endTimeValue.year, endTimeValue.month, endTimeValue.day, endTimeValue.hour, endTimeValue.minute, endTimeValue.second);
+        return temp;
+    }
+
+    public EndTime SetNow(DateTime now)
+    {
+        EndTime temp = new EndTime(now.Month, now.Day, now.Year, now.Hour, now.Minute, now.Second);
+        return temp;
+    }
 
     public User User;
 
@@ -143,7 +245,7 @@ public class DataBaseManager : MonoBehaviour
         string json = JsonUtility.ToJson(ItemDataJson);
 
         //경로
-        string path = "USER_INFO/" + FirebaseAuth.DefaultInstance.CurrentUser.UserId + "/MAP_INFO";
+        string path = "USER_INFO/" + FirebaseAuth.DefaultInstance.CurrentUser.UserId +"/MAP_INFO";
         string pathF = "USER_INFO/" + User.nickName + "/";
         var task = dataBase.GetReference(path).SetRawJsonValueAsync(json);
         yield return new WaitUntil(() => task.IsCompleted);
@@ -186,7 +288,7 @@ public class DataBaseManager : MonoBehaviour
             {
                 print("맵 정보 로드 실패");
 
-                SaveUser((result) =>
+                SaveMap((result) =>
                 {
                     // Firebase와 통신 끝나면 들어온다
                     complete(true);
--- Assets/Hyo_Folder/Script/GameManager_HYO.cs
+++ Assets/Hyo_Folder/Script/GameManager_HYO.cs
@@ -2,23 +2,113 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
 
 public class GameManager_HYO : MonoBehaviour
 {
+    
     public Text nickName;
     public Text mapName;
 
-
     void Start()
     {
+        print(DateTime.Now);
+       
+
+
+        //시간 차 계산 (coin)
+        DataBaseManager.instance.GetTimeCoin(GetCoinText);
+
+
+        //Get UserInfo
         nickName.text = DataBaseManager.instance.User.nickName;
         mapName.text = DataBaseManager.instance.User.mapName;
 
         DataBaseManager.instance.GetMapInfo(GetMap);
+
     }
 
-    
-    
+
+    //coin 
+    public int goneTime;
+    public int myCurrCoinJson;
+    public int myCoin;
+    public int getCoin = 10;
+    public int coinPerM = 1;
+    public Text myCoinText;
+
+    public void CoinSystem()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitinfo;
+
+        if (Physics.Raycast(ray, out hitinfo))
+        {
+            if(hitinfo.transform.tag == "Animal")
+            {
+                myCoin += getCoin;
+            }
+        }
+    }
+    void GetCoinText(bool result)
+    {
+        TimeSpan timeSpan;
+        DateTime userIndate;
+        DateTime start;
+
+        start = DateTime.Now;
+        userIndate = DataBaseManager.instance.SetDate(DataBaseManager.instance.exitTimeJson);
+        print("원래 있던거: " + userIndate);
+        timeSpan = start - userIndate;
+        goneTime = (int)timeSpan.TotalMinutes; //minutes Diff
+        print("한번 봅시다  - " + goneTime);
+
+        myCurrCoinJson = DataBaseManager.instance.exitTimeJson.myCurrCoin;
+        myCoin += myCurrCoinJson + (goneTime * coinPerM);
+
+        //coin Tostring()
+        string coinString = myCoin.ToString();
+        myCoinText.text = coinString;
+
+        //뒤로가기 누르면 종료 로딩 화면
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                SceneManager.LoadScene("LodingScene");
+            }
+        }
+    }
+    public void OnclickGameExit()
+    {
+        print("coin save");
+        DateTime exit = DateTime.Now;
+
+        //DataBaseManager.instance.exitTimeJson = new EndTime(exit.Month, exit.Year, exit.Day, exit.Hour, exit.Minute, exit.Second);
+        DataBaseManager.instance.exitTimeJson = DataBaseManager.instance.SetNow(DateTime.Now);
+
+        //DataBaseManager.instance.exitTimeJson.exitTime = exit;
+        DataBaseManager.instance.exitTimeJson.myCurrCoin = myCoin;
+        DataBaseManager.instance.SaveTimeCoin(GetCoinText);
+    }
+
+    //inventory
+    public GameObject inventory;
+    public void OnclickInventory()
+    {
+        if(inventory.activeSelf == false)
+        {
+            inventory.SetActive(true);
+        }
+        else
+        {
+            inventory.SetActive(false);
+        }
+        
+
+    }
+
     //itemPlace
     void GetMap(bool result)
     {
@@ -35,22 +125,47 @@ public class GameManager_HYO : MonoBehaviour
 
     }
 
+    //select item
     public GameObject[] items;
+    public Button[] itemBtns;
     public int selectItem;
+
+    //touch
+    private Touch tempTouchs;
+    private Vector3 touchedPos;
+
+    private bool touchOn;
+
+    public void SelectItemBtn()
+    {
+        for(int i = 0; i < itemBtns.Length; i++)
+        {
+            if(itemBtns[i] == gameObject)
+            {
+                selectItem = i;
+            }
+        }
+    }
+  
     void Update()
     {
+        //coin text
+        string coinString = myCoin.ToString();
+        myCoinText.text = coinString;
 
-        if (Input.GetKeyDown(KeyCode.Alpha1)) selectItem = 0;
-        if (Input.GetKeyDown(KeyCode.Alpha2)) selectItem = 1;
-        if (Input.GetKeyDown(KeyCode.Alpha3)) selectItem = 2;
-        if (Input.GetKeyDown(KeyCode.Alpha4)) selectItem = 3;
-        if (Input.GetKeyDown(KeyCode.Alpha5)) selectItem = 4;
-        if (Input.GetKeyDown(KeyCode.Alpha6)) selectItem = 5;
+       
+        //if (Input.GetKeyDown(KeyCode.Alpha1)) selectItem = 0;
+        //if (Input.GetKeyDown(KeyCode.Alpha2)) selectItem = 1;
+        //if (Input.GetKeyDown(KeyCode.Alpha3)) selectItem = 2;
+        //if (Input.GetKeyDown(KeyCode.Alpha4)) selectItem = 3;
+        //if (Input.GetKeyDown(KeyCode.Alpha5)) selectItem = 4;
+        //if (Input.GetKeyDown(KeyCode.Alpha6)) selectItem = 5;
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(1))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            touchOn = false;
 
             if (Physics.Raycast(ray, out hit))
             {
@@ -59,9 +174,34 @@ public class GameManager_HYO : MonoBehaviour
                 ItemInfo data = new ItemInfo();
                 data.type = selectItem;
                 data.pos = hit.point;
+
                 data.rot = item.transform.eulerAngles;
 
                 DataBaseManager.instance.itemDatas.Add(data);
+
+                //touchOn = false;
+
+                //if (Input.touchCount > 0)
+                //{    //터치가 1개 이상이면
+                //    for (int i = 0; i < Input.touchCount; i++)
+                //    {
+                //        tempTouchs = Input.GetTouch(i);
+                //        //touch phase
+                //        if (tempTouchs.phase == TouchPhase.Began)
+                //        {
+                //            //Get world position
+                //            touchedPos = Camera.main.ScreenToWorldPoint(tempTouchs.position);
+                //            touchOn = true;
+                //        }
+                //        else if (tempTouchs.phase == TouchPhase.Ended)
+                //        {   
+                            
+
+                //            break;   
+                //        }
+                //    }
+                //}
+                
             }
         }
     }
